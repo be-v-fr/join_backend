2422d5276fe09d629f1c4e187d140bd864d9d46a
Bengt Früchtenicht <befru000@gmail.com>
2024-09-15 10:10:36 +0200

    fix: deleted unnecessary code

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
