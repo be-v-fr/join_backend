42ec4a5301f549e6814ca599e76be618f3e2e2b3
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-01-30 15:50:37 +0100

    feat: configured static

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from secret_keys import DJANGO_SECRET_KEY
 
@@ -138,7 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticfiles')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
