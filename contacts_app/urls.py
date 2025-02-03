88da466f0a7f2b1307de689bc08edb4eb0d51c1c
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-02-03 12:56:05 +0100

    refactor: optimized urls

--- contacts_app/urls.py
+++ contacts_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ContactsView
 
 urlpatterns = [
-    path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('contacts/<int:pk>/', ContactsView.as_view(), name='contact'),
+    path('', ContactsView.as_view(), name='contacts'),
+    path('<int:pk>/', ContactsView.as_view(), name='contact'),
 ]
--- tasks_app/urls.py
+++ tasks_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import TasksView, SubtasksView, tasks_stream, subtasks_stream
 
+SUBTASKS_URL = 'subtasks/'
+
 urlpatterns = [
-    path('tasks/', TasksView.as_view(), name='tasks'),
-    path('tasks/<int:pk>/', TasksView.as_view(), name='task'),
-    path('tasks/stream/', tasks_stream, name='tasks_stream'),
-    path('subtasks/', SubtasksView.as_view(), name='subtasks'),
-    path('subtasks/<int:pk>/', SubtasksView.as_view(), name='subtask'),
-    path('subtasks/stream/', subtasks_stream, name='subtasks_stream'),
+    path('', TasksView.as_view(), name='tasks'),
+    path('<int:pk>/', TasksView.as_view(), name='task'),
+    path('stream/', tasks_stream, name='tasks_stream'),
+    path(SUBTASKS_URL, SubtasksView.as_view(), name='subtasks'),
+    path(SUBTASKS_URL + '<int:pk>/', SubtasksView.as_view(), name='subtask'),
+    path(SUBTASKS_URL + 'stream/', subtasks_stream, name='subtasks_stream'),
 ]
