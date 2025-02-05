fd3e1cc29b5d431734fb25d423aa6bbb785d2a86
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-02-05 14:37:43 +0100

    fix: cors settings

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -38,8 +38,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
     'http://127.0.0.1:4200',
 ]
-CORS_ALLOW_METHODS = ["GET", "OPTIONS"]
-CORS_ALLOW_HEADERS = ["*"]
 
 
 # Application definition
